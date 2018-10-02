ed8f4fbfb41ac59ccf12e1c1a27c8178432199d7
Irene Baraldi <ire.baraldi@hotmail.it>
2018-10-02 11:30:54 +0200

    esercizio02-finito

--- sketch.js
+++ sketch.js
@@ -3,9 +3,64 @@ function preload(){
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(800,600);
+  background(40);
+  angleMode(DEGREES);
+  frameRate(12);
 }
 
 function draw() {
-  // put drawing code here
+  push();
+  strokeWeight(0.2);
+  stroke(color('#606060'),100);
+  translate(400,300);
+  rotate(frameCount*10);
+  noFill();
+  ellipse(100,0,200,200);
+  pop();
+
+  push();
+  translate(400,300);
+  //rotate(frameCount*3);
+  noFill();
+
+  //stroke(lerpColor(color('#FF7F00'), color('#0000FF'), frameCount/120));
+  //stroke(lerpColor(color('#0000FF'), color('#FF7F00'), frameCount/120));
+  stroke(192, 192, 192, 20);
+  line(cos(frameCount*3)*100,sin(frameCount*3)*100,200,0,);
+
+  //stroke(lerpColor(color('#0000FF'), color('#FF7F00'), frameCount/120));
+  stroke(255, 229, 204, 20);
+  line(0,200,cos(frameCount*3)*100,sin(frameCount*3)*100);
+
+  //stroke(lerpColor(color('#0000FF'), color('#FF7F00'), frameCount/120));
+  stroke(255, 229, 204, 20);
+  line(0,-200,cos(frameCount*3)*100,sin(frameCount*3)*100);
+
+  //stroke(lerpColor(color('#0000FF'), color('#FF7F00'), frameCount/120));
+  stroke(255, 229, 204, 20);
+  line(-200,0,cos(frameCount*3)*100,sin(frameCount*3)*100);
+
+  pop();
+
+  push();
+  translate(400,300);
+  stroke(lerpColor(color('#FF9933'), color('#3399FF'), frameCount/120));
+  noFill();
+  line(100,0,cos(frameCount*3)*100,sin(frameCount*3)*100);
+  pop();
+
+  translate(400,300);
+  rotate(frameCount*3);
+  stroke(224, 10);
+  line(800,10,cos(frameCount)*100,sin(frameCount)*100);
+
+
+
+
+
+  //Stops the draw function if a certain condition avaluates true
+  if (frameCount == 120) {
+    noLoop();
+  }
 }
